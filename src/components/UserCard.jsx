d68dd203760ba3e1722f8ded75ddf36059651d51
mitulgadhiya3706 <mitulgadhiya23@gmail.com>
2026-07-25 17:10:59 +0530

    Handle feed appearence

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
+
 const UserCard = ({ user }) => {
     if (!user) return null;
 
     const { firstName, lastName, age, about, gender, photoUrl, skills, gitHubUrl, linkedInUrl, isPremium } = user;
+    const dispatch = useDispatch();
+
+    const handleSendRequest = async (status, userId) => {
+        try {
+            const res = axios.post(BASE_URL + "/request/send/" + status + "/" + userId,
+                {},
+                { withCredentials: true }
+            );
+            dispatch(removeUserFromFeed(userId));
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <>
@@ -22,8 +40,16 @@ const UserCard = ({ user }) => {
                     {skills && <p>{skills}</p>}
                     <p>{about}</p>
                     <div className="card-actions justify-center my-4">
-                        <button className="btn btn-primary">Interested</button>
-                        <button className="btn btn-secondary">Ignore</button>
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => handleSendRequest("intersted", user._id)}
+                        >Interested
+                        </button>
+                        <button
+                            className="btn btn-secondary"
+                            onClick={() => handleSendRequest("ignore", user._id)}
+                        >Ignore
+                        </button>
                     </div>
                 </div>
             </div>
